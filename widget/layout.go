64c13501e9070d8c65d041082e322d671f035595
Arjen Brouwer <arjen@ag5.com>
2023-10-27 14:45:31 +0200

    Removed

--- widget/layout.go
+++ widget/layout.go
@@ -1,18 +1,2 @@
 package widget
 
-import (
-	"gioui.org/layout"
-	"gioui.org/unit"
-)
-
-func VSpacer(m unit.Dp) layout.Widget {
-	return func(gtx layout.Context) layout.Dimensions {
-		return layout.Spacer{Height: m}.Layout(gtx)
-	}
-}
-
-func HSpacer(m unit.Dp) layout.Widget {
-	return func(gtx layout.Context) layout.Dimensions {
-		return layout.Spacer{Width: m}.Layout(gtx)
-	}
-}
