b4ccd43fe97ac5eb90e6cf7a8c4a032065a0b974
Arjen Brouwer <arjen@ag5.com>
2023-10-27 14:44:44 +0200

    Use system fonts

---
+++ fonts.go
@@ -0,0 +1,32 @@
+package cu
+
+import (
+	_ "embed"
+	"gioui.org/font"
+	"gioui.org/font/opentype"
+	"gioui.org/text"
+)
+
+var th *Theme
+
+//go:embed fonts/FiraCode-Regular.ttf
+var monoFontRegularData []byte
+
+//go:embed fonts/FiraCode-Bold.ttf
+var monoFontSemiBoldData []byte
+
+// var monoFontLight text.FontFace
+var monoFontRegular text.FontFace
+
+// var monoFontMedium text.FontFace
+var monoFontBold text.FontFace
+
+func init() {
+	// Initialize fonts
+
+	face, _ := opentype.Parse(monoFontRegularData)
+	monoFontRegular = text.FontFace{Font: font.Font{Typeface: "monospace", Weight: font.Normal}, Face: face}
+
+	face, _ = opentype.Parse(monoFontSemiBoldData)
+	monoFontBold = text.FontFace{Font: font.Font{Typeface: "monospace", Weight: font.Bold}, Face: face}
+}
